7b9424a75d6f7a5ebe2e2c763ad9dce4be0d50b6
jinwoo Lee <jinwoolee@jinwooui-noteubug.local>
2024-02-14 10:10:48 +0900

    test222

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -2,8 +2,9 @@ package com.example.demo;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class HomeController {
     @GetMapping("/")
     public String test() {
